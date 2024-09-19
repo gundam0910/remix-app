a36be9b388e26500f40de95047e97b2785315566
geoffrey <geoffrey.ayap@98labs.com>
2024-09-19 20:55:30 +0800

    update and modal

--- app/components/updateForm.tsx
+++ app/components/updateForm.tsx
@@ -1,47 +1,115 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Form } from "@remix-run/react";
-import { useState } from "react";
+import { Form } from '@remix-run/react'
+import { useState } from 'react'
 
 export default function UpdateBookForm({
-  book,
-  onClose,
+    book,
+    onClose,
 }: {
-  book: any;
-  onClose: any;
+    book: any
+    onClose: any
 }) {
-  const [title, setTitle] = useState(book.title);
-  const [author, setAuthor] = useState(book.author);
-  return (
-    <div className="overlay">
-      <div className="modal">
-        <h2>Update Book</h2>
-        <Form method="patch" action={"/books?id=" + book.id}>
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
-          <br />
-          <label htmlFor="author">Author</label>
-          <input
-            type="text"
-            id="author"
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            required
-          />
-          <br />
-          <button type="submit">Update</button>
-          <button type="button" onClick={onClose}>
-            Close
-          </button>
-        </Form>
-      </div>
-    </div>
-  );
+    const [title, setTitle] = useState(book.title)
+    const [author, setAuthor] = useState(book.author)
+    const [description, setDescription] = useState(book.description)
+    return (
+        <div
+            className="relative z-10"
+            aria-labelledby="modal-title"
+            role="dialog"
+            aria-modal="true"
+        >
+            <div
+                className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                aria-hidden="true"
+            ></div>
+
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <div className="relative transform overflow-hidden rounded-lg bg-black text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <Form
+                        method="patch"
+                        className="p-4 md:p-5"
+                        action={'/books?id=' + book.id}
+                    >
+                        <div className="grid gap-4 mb-4 grid-cols-2">
+                              <div className="col-span-2">
+                                <label
+                                    htmlFor="title"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Title
+                                </label>
+                                <input
+                                    type="text"
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                    name="title"
+                                    id="title"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                    placeholder="Type product name"
+                                    required
+                                />
+                              </div>
+                            <div className="col-span-2">
+                                <label
+                                    htmlFor="author"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Author
+                                </label>
+                                <input
+                                    type="text"
+                                    value={author}
+                                    onChange={(e) => setAuthor(e.target.value)}
+                                    name="author"
+                                    id="author"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                    placeholder="Type product name"
+                                    required
+                                />
+                            </div>
+                            <div className="col-span-2">
+                                <label
+                                    htmlFor="description"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Description
+                                </label>
+                                <input
+                                    type="text"
+                                    value={description}
+                                    onChange={(e) =>
+                                        setDescription(e.target.value)
+                                    }
+                                    name="description"
+                                    id="description"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                    placeholder="Write product description here"
+                                    required
+                                />
+                            </div>
+                          </div>
+                            <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-2">
+                                <button
+                                    type="submit"
+                                    className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-300 hover:text-black sm:ml-3 sm:w-auto"
+                                >
+                                    Update
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={onClose}
+                                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+    )
 }
--- app/routes/books._index/route.tsx
+++ app/routes/books._index/route.tsx
@@ -1,10 +1,12 @@
 import { Form, json, useLoaderData } from '@remix-run/react';
+import { LinksFunction } from '@remix-run/node';
 import { useState } from 'react';
-import { redirectWithSuccess, redirectWithError } from "remix-toast";
+import { jsonWithSuccess, jsonWithError } from "remix-toast";
 
-import {getBooks, createBook, deleteBook} from '../../controllers/books'
+import {getBooks, createBook, deleteBook, updateBook} from '../../controllers/books'
 import CreateBookForm from '../../components/createForm';
 import UpdateBookForm from '../../components/updateForm';
+import "./formOverlay.css?url";
 
 export const loader = async () => {
   const response = await getBooks();
@@ -14,19 +16,22 @@ export const loader = async () => {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function action({ request }: { request: any }) {
+  console.log('request.method', request.method)
   if (request.method === "DELETE") {
     const url = new URL(request.url);
     const bookId = url.searchParams.get("id");
     if (bookId) {
       const bookData = await deleteBook(bookId);
       if (bookData) {
-        return redirectWithSuccess("/books", "You have successfully deleted!"); 
+        return jsonWithSuccess({ result: "Data saved successfully" }, { message: "You have successfully deleted!"}); 
       } else {
-        return redirectWithError("/books", "Delete failed.");
+        return jsonWithError(null, "Delete failed.");
       }
     }
     
-  }else{
+  } else if (request.method === "PATCH") {
+    const url = new URL(request.url);
+    const bookId = url.searchParams.get("id");
     const formData = await request.formData();
     const title = formData.get("title");
     const author = formData.get("author");
@@ -36,15 +41,36 @@ export async function action({ request }: { request: any }) {
       typeof title !== 'string' ||
       typeof author !== 'string'
     ) {
-      return redirectWithError("/books", "Form not submitted correctly.");
+      return jsonWithError(null, "Form not submitted correctly.");
+    }
+
+    const fields = { title, author, description };
+    if (!bookId) return jsonWithError(null, "Not valid book id.");
+    const bookData = await updateBook(bookId, fields);
+    if (bookData) {
+      return jsonWithSuccess({ result: "Data updated successfully" }, { message: "You have successfully updated!"}); 
+    } else {
+      return jsonWithError(null, "Fail to update book.");
+    }
+  } else {
+    const formData = await request.formData();
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const description = formData.get("description");
+
+    if (
+      typeof title !== 'string' ||
+      typeof author !== 'string'
+    ) {
+      return jsonWithError(null, "Form not submitted correctly.");
     }
 
     const fields = { title, author, description };
     const bookData = await createBook(fields);
     if (bookData) {
-      return redirectWithSuccess("/books", "You have successfully created!"); 
+      return jsonWithSuccess({ result: "Data saved successfully" }, { message: "You have successfully created!"}); 
     } else {
-      return redirectWithError("/books", "Fail to create book.");
+      return jsonWithError(null, "Fail to create book.");
     }
     
   }
